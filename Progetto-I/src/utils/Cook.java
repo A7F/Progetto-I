9a9ea04af415737cd0e51f78dc5fc9d6005bde4b
utente <utente@LucaPC>
2016-05-10 22:39:21 +0200

    fix javadoc, aggiunto metodo per ripristinare ordine cancellato

--- Progetto-I/src/utils/Cook.java
+++ Progetto-I/src/utils/Cook.java
@@ -1,27 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package utils;
 
 import java.util.ArrayList;
 
 /**
- *
+ * Gestione in cucina
+ * ordersCopy è una copia dell' arraylist orders siccome non voglio che il cuoco elimini
+ * elementi di ordine sul server. In sostanza funziona solo da promemoria
  * @author Luca :3
+ * @param name,orders
  */
 public class Cook {
     private ArrayList<Order> ordersCopy;
     private String cookName;
+    private ArrayList<Order> ordersDeleted;
     
     public Cook(String name,ArrayList<Order> orders){
     this.cookName = name;
     ordersCopy = orders;
 }
-    
+    /**
+     * Imposta come eseguito un ordine
+     * @author Luca ^^
+     * @param order 
+     */
     private void setDone(Order order){
         ordersCopy.remove(order);
+        ordersDeleted.add(order);
     }
     
+    /**
+     * Ripristina l'ultimo ordine segnato come eseguito. Gestione FIFO
+     * @author Luca :)
+     */
+    private void restoreDone(){
+        Order tmp;
+        tmp = ordersDeleted.get(ordersDeleted.size()-1);
+        ordersDeleted.remove(tmp);
+        ordersCopy.set(1, tmp);
+    }
 }
