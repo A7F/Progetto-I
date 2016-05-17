bd315a07680d13377e08d4322122d3e9d9bde76a
Fabio <FabioTagliani>
2016-05-17 14:18:28 +0200

    Creati addOrder e RemoveOrder con relativo test (Test02)

---
+++ Progetto-I/src/tests/Test02.java
@@ -0,0 +1,34 @@
+/*
+ * 
+ */
+package tests;
+
+import java.io.IOException;
+import menu.Menu;
+import menu.MenuElement;
+import utils.Order;
+import utils.Restaurant;
+
+/**
+ * Test per il funzionamento di addOrder e removeOrder.
+ * @author fabio
+ */
+public class Test02 {
+    public static void main(String[] args) throws IOException {
+        Restaurant diablo = new Restaurant("Diablo", 5,"./data/menu.txt");
+
+       
+
+        System.out.println(diablo.getMenu());
+        
+        MenuElement me = new MenuElement("Pasta alla Carbonara", 7, "PRIMO", "Pasta alla Carbonara	7	PRIMO	Spaghetti con carbonara, servita calda col ghiaccio e un pizzico di roccia saporita al ragno.\n");
+        Order order = new Order(1,me, "al dente");          
+        diablo.addOrder(1, order);
+        diablo.addOrder(1, order);
+        System.out.println(diablo.getTables());
+        
+        diablo.removeOrder(1, order);
+        System.out.println(diablo.getTables());
+    }
+    
+}
