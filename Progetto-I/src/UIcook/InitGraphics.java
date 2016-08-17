a3b2b71a0226aa91c063966d7ae032b694af9c93
luca <rockthisway05@gmail.com>
2016-08-18 00:48:25 +0200

    Aggiunto login, manager login e utility aggiunta user al database

--- Progetto-I/src/UIcook/InitGraphics.java
+++ Progetto-I/src/UIcook/InitGraphics.java
@@ -22,19 +22,26 @@ public class InitGraphics {
 
     private void initUi() {
         JFrame frame = new JFrame("Cucina");
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        
         list =new JList(elements.toArray());
         list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         list.setLayout(new FlowLayout());
         list.setCellRenderer(new CookListCellRender());
+        
         JScrollPane panel = new JScrollPane(list);
         panel.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         panel.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        
         ButtonPanel buttonPanel = new ButtonPanel(elements,list);
         buttonPanel.setLayout(new FlowLayout());
+        
         frame.add(buttonPanel,BorderLayout.NORTH);
         frame.add(panel,BorderLayout.CENTER);
         frame.setVisible(true);
         frame.setSize(600, 400);
+        frame.setResizable(false);
+        frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 }
--- Progetto-I/src/UIlogin/NewUserWindow.java
+++ Progetto-I/src/UIlogin/NewUserWindow.java
@@ -5,10 +5,39 @@
  */
 package UIlogin;
 
+import java.awt.*;
+import javax.swing.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
 /**
- *
+ * questa classe è un pannello con cui il gestore può aggiungere nuovi impiegati al database
  * @author Luca
  */
-public class NewUserWindow {
+class NewUserWindow {
+    FormPanel form = new FormPanel();
+    JFrame frame = new JFrame("Nuovo Utente");
+    JButton b1 = new JButton("Inserisci");
+    JButton b2 = new JButton("Annulla");
+    JPanel p1 = new JPanel(new FlowLayout());
     
+    public NewUserWindow(){
+        initComponents();
+    }
+    
+    private void initComponents(){
+        frame.setLayout(new BorderLayout());
+        frame.add(form,BorderLayout.CENTER);
+        
+        //TODO: aggiungere action listener dei bottoni modificando il costruttore
+        
+        p1.add(b1);
+        p1.add(b2);
+        
+        frame.add(p1,BorderLayout.SOUTH);
+        
+        frame.pack();
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
 }
