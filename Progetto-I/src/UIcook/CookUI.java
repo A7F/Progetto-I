06fd28815066355eb82d26aef9c163662c892376
luca <rockthisway05@gmail.com>
2016-10-03 11:19:39 +0200

    Migliorata UI cuoco

--- Progetto-I/src/UIcook/CookUI.java
+++ Progetto-I/src/UIcook/CookUI.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 import javax.swing.*;
-import static javax.swing.JFrame.*;
+import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import restaurant.Order;
 import restaurant.Restaurant;
 
@@ -21,6 +23,13 @@ public class CookUI implements Observer{
     DefaultListModel model=new DefaultListModel();
     JFrame frame = new JFrame("Cucina");
     Restaurant restaurant;
+    JPanel descriptionPanel;
+    JPanel ingredientsPanel;
+    JPanel lowContainer = new JPanel(new GridLayout(2,1));
+    JLabel noteLabel = new JLabel("NOTE >> ");
+    JLabel ingredientiLabel = new JLabel("INGREDIENTI >> ");
+    JLabel ingredientiText = new JLabel();
+    JLabel noteText = new JLabel();
     
     public CookUI(Restaurant r,int userId){
         this.userId=userId;
@@ -30,7 +39,7 @@ public class CookUI implements Observer{
         initUi();
     }
 
-    private void initUi() {
+    private void initUi(){
         frame.setJMenuBar(new utils.MenuBar(frame,userId));
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         
@@ -38,10 +47,25 @@ public class CookUI implements Observer{
             model.addElement(elements.get(i));
         }
         
+        createLowerPanel();
+        
         list =new JList(model);
         list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         list.setLayout(new FlowLayout());
+        
         list.setCellRenderer(new CookListCellRender());
+        list.addListSelectionListener(new ListSelectionListener(){
+            @Override
+            public void valueChanged(ListSelectionEvent e){
+                if(list.getSelectedIndex()==-1){
+                    noteText.setText("Non hai selezionato niente");
+                    ingredientiText.setText("Non hai selezionato niente");
+                }else{
+                    noteText.setText(elements.get(list.getSelectedIndex()).getNotes());
+                    ingredientiText.setText(elements.get(list.getSelectedIndex()).getMenuElement().getDescription());
+                }
+            }
+        });
         
         JScrollPane panel = new JScrollPane(list);
         panel.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
@@ -52,15 +76,18 @@ public class CookUI implements Observer{
         
         frame.add(buttonPanel,BorderLayout.NORTH);
         frame.add(panel,BorderLayout.CENTER);
+        frame.add(lowContainer,BorderLayout.SOUTH);
         frame.setVisible(true);
         frame.setSize(600, 400);
         frame.setResizable(false);
         frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
         frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        
     }
 
     @Override
     public void update(Observable o, Object arg) {
+        descriptionPanel.repaint();
         elements=restaurant.getOrdersArray();
         model.removeAllElements();
         for(int i=0;i<elements.size();i++){
@@ -68,4 +95,16 @@ public class CookUI implements Observer{
         }
         list.repaint();
     }
+
+    private void createLowerPanel() {
+        descriptionPanel = new JPanel(new FlowLayout());
+        ingredientsPanel = new JPanel(new FlowLayout());
+        descriptionPanel.add(noteLabel);
+        descriptionPanel.add(noteText);
+        ingredientsPanel.add(ingredientiLabel);
+        ingredientsPanel.add(ingredientiText);
+        lowContainer.add(descriptionPanel);
+        lowContainer.add(ingredientsPanel);
+    }
+
 }
--- Progetto-I/src/tests/TestCookUI.java
+++ Progetto-I/src/tests/TestCookUI.java
@@ -18,7 +18,7 @@ public class TestCookUI {
         MenuElement me = new MenuElement("Pasta alla Carbonara", 7, "PRIMO", "Descrizione.");
         MenuElement me2 = new MenuElement("Pasta alla Carbonara", 7, "PRIMO", "Altra descrizione.");
         Order order = new Order(1,me, "al dente");
-        Order order2 = new Order(1,me, "al dente");
+        Order order2 = new Order(1,me, "buona eh");
         diablo.addOrder(1, order);
         diablo.addOrder(1, order2);
         
