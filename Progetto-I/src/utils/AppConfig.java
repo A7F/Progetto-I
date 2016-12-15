f06b0d7e00a5fbe17767c76c150a191ebeec0015
luca <rockthisway05@gmail.com>
2016-12-15 13:17:26 +0100

    fix javadoc

--- Progetto-I/src/utils/AppConfig.java
+++ Progetto-I/src/utils/AppConfig.java
@@ -33,6 +33,11 @@ public class AppConfig {
         restaurantName = json.getString("restaurant_name");
     }
     
+    /**
+     * metodo singleton, ritorna istanza di classe
+     * @return istanza della classe
+     * @author Luca
+     */
     public static AppConfig getInstance(){
         if(instance==null){
             instance = new AppConfig();
@@ -40,7 +45,13 @@ public class AppConfig {
         return instance;
     }
 
-    public static String readFile() {
+    /**
+     * metodo per leggere il file di configurazione dell'applicazione. La stringa di ritorno
+     * verrà convertita in JSONObject
+     * @return file come String
+     * @author Luca
+     */
+    private static String readFile() {
         String result = "";
         try {
             BufferedReader br = new BufferedReader(new FileReader("./data/appConfig.json"));
@@ -57,6 +68,10 @@ public class AppConfig {
         return result;
     }
     
+    /**
+     * metodo per salvare i cambiamenti (eventuali) sul file json
+     * @author Luca
+     */
     private void writeFile() {
         try {
             FileWriter file = new FileWriter("./data/appConfig.json");
@@ -71,51 +86,96 @@ public class AppConfig {
         }
     }
 
+    /**
+     * @author Luca
+     * @return nome del progetto
+     */
     public String getProjectName(){
         return projectName;
     }
 
+    /**
+     * @author Luca
+     * @param projectName nome del progetto
+     */
     public void setProjectName(String projectName) {
         this.projectName = projectName;
         json.put("project_name", projectName);
     }
 
+    /**
+     * @author Luca
+     * @return nome dell'applicazione
+     */
     public String getAppName() {
         return appName;
     }
 
+    /**
+     * @author Luca
+     * @param appName nome dell'applicazione
+     */
     public void setAppName(String appName) {
         this.appName = appName;
         json.put("app_name", appName);
     }
 
+    /**
+     * @author Luca
+     * @return numero dei davoli di default
+     */
     public int getTableNumber() {
         return tableNumber;
     }
 
+    /**
+     * @author Luca
+     * @param tableNumber numero dei tavoli di default
+     */
     public void setTableNumber(int tableNumber) {
         this.tableNumber = tableNumber;
         json.put("table_number", tableNumber);
     }
 
+    /**
+     * @author Luca
+     * @return percorso del file del menu
+     */
     public String getMenuPath() {
         return menuPath;
     }
 
+    /**
+     * @author Luca
+     * @param menuPath percorso file del menu
+     */
     public void setMenuPath(String menuPath) {
         this.menuPath = menuPath;
         json.put("menu_path", menuPath);
     }
 
+    /**
+     * @author Luca
+     * @return nome del ristorante
+     */
     public String getRestaurantName() {
         return restaurantName;
     }
 
+    /**
+     * @author Luca
+     * @param restaurantName nome del ristorante
+     */
     public void setRestaurantName(String restaurantName) {
         this.restaurantName = restaurantName;
         json.put("restaurant_name", restaurantName);
     }
     
+    /**
+     * metodo per scrivere le modifiche su file di configurazione
+     * @author Luca
+     * @see writeFile
+     */
     public void commitChanges(){
         writeFile();
     }
