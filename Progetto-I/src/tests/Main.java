0a067e1fced2eb1c3771ca35155dbafc5c426cac
luca <rockthisway05@gmail.com>
2016-12-18 19:06:42 +0100

    aggiunta classe main

---
+++ Progetto-I/src/tests/Main.java
@@ -0,0 +1,24 @@
+package tests;
+
+import UIlogin.LoginUI;
+import java.io.IOException;
+import restaurant.Restaurant;
+import utils.AppConfig;
+
+/**
+ * classe main dell'applicazione
+ * @author Luca
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Restaurant r = null;
+        try {
+            r = new Restaurant(AppConfig.getInstance().getRestaurantName(),AppConfig.getInstance().getTableNumber(),AppConfig.getInstance().getMenuPath());
+            LoginUI login = new LoginUI(r);
+        } catch (IOException ex) {
+            LoginUI login = new LoginUI(r);
+        }
+    }
+    
+}
