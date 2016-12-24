bb4f9aae23d6a945da510ba1685b8c760e693010
luca <rockthisway05@gmail.com>
2016-12-24 17:27:24 +0100

    aggiunte nuove tabelle database

--- Progetto-I/src/utils/DatabaseManager.java
+++ Progetto-I/src/utils/DatabaseManager.java
@@ -49,6 +49,8 @@ public class DatabaseManager {
             statement.executeUpdate(DatabaseStrings.createImpiegati);
             statement.executeUpdate(DatabaseStrings.createTavoli);
             statement.executeUpdate(DatabaseStrings.createPrenotazioni);
+            statement.executeUpdate(DatabaseStrings.createOrders);
+            statement.executeUpdate(DatabaseStrings.createSnapshot);
             statement.executeUpdate(DatabaseStrings.alterPrenotazioni);
             System.out.println(">> Database e tabelle generate con successo");
         } catch (SQLException e) {
