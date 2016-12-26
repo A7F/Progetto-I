21958b83a84f10cc0c8c1a1731b69f85bce6ec6b
luca <rockthisway05@gmail.com>
2016-12-26 16:07:48 +0100

    iniziato metodo thread snapshot

--- Progetto-I/src/snapshotEngine/SnapshotDaemon.java
+++ Progetto-I/src/snapshotEngine/SnapshotDaemon.java
@@ -1,5 +1,10 @@
 package snapshotEngine;
 
+import hibernateMappers.HibernateUtil;
+import hibernateMappers.OrderSave;
+import hibernateMappers.Snapshot;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import restaurant.Restaurant;
 
 /**
@@ -15,10 +20,25 @@ class SnapshotDaemon implements Runnable{
     
     @Override
     public void run() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
         for(int i=0; i<restaurant.getTables().size(); i++){
             
-            
+            if(!restaurant.getTables().get(i).getOrdersArray().isEmpty()){
+                Snapshot snap = new Snapshot(restaurant.getTables().get(i).getTableId(),restaurant.getTables().get(i).getIsTaken());
+                tx = session.beginTransaction();
+                int id = (Integer)session.save(snap);
+                
+                for(int j=0; j<restaurant.getTables().get(i).getOrdersArray().size(); j++){
+                    OrderSave order = new OrderSave(restaurant.getTables().get(i).getOrdersArray().get(j).getMenuElement().getId(),restaurant.getTables().get(i).getOrdersArray().get(j).getNotes());
+                    session.save(order);
+                }
+                
+            }
             
         }
+        tx.commit();
+        tx=null;
+        session.close();
     }
 }
