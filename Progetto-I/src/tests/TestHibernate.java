a65403fcc1ecb3eb62843f37dee5abd7ae2b457a
luca <rockthisway05@gmail.com>
2016-12-27 12:19:58 +0100

    update classe test hibernate

--- Progetto-I/src/tests/TestHibernate.java
+++ Progetto-I/src/tests/TestHibernate.java
@@ -1,12 +1,7 @@
 package tests;
 
-import hibernateMappers.HibernateUtil;
-import hibernateMappers.Snapshot;
 import java.io.IOException;
 import menu.MenuElement;
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
 import restaurant.Order;
 import restaurant.Restaurant;
 import snapshotEngine.SnapshotEngine;
@@ -29,33 +24,15 @@ public class TestHibernate {
         r.addOrder(1, order);
         r.addOrder(1, order2);
         
-//        Session session = HibernateUtil.getSessionFactory().openSession();
-//        Transaction tx = null;
-//        
-//        Snapshot snap = new Snapshot(2,1);
-//        Snapshot snap2 = new Snapshot(45,24);
-//        Snapshot snap3 = new Snapshot(45,24,"buono");
-//        
-//        try{
-//            tx = session.beginTransaction();
-//            int id = (Integer)session.save(snap);
-//            System.out.println("id1= "+id);
-//            id = (Integer)session.save(snap2);
-//            System.out.println("id2= "+id);
-//            id = (Integer)session.save(snap3);
-//            System.out.println("id2= "+id);
-//            tx.commit();
-//        }catch(HibernateException e){
-//            tx.rollback();
-//            e.printStackTrace();
-//        }finally{
-//            session.close();
-//        }
-        
-        SnapshotEngine engine = SnapshotEngine.getInstance();
-        engine.setMilliseconds(6000);
-        engine.setRestaurant(r);
-        engine.startDaemon();
+        if(AppConfig.getInstance().getExitState()!=0){
+            SnapshotEngine engine = SnapshotEngine.getInstance();
+            engine.restore();
+        }else{
+            SnapshotEngine engine = SnapshotEngine.getInstance();
+            engine.setMilliseconds(6000);
+            engine.setRestaurant(r);
+            engine.startDaemon();
+        }
         
     }
     
