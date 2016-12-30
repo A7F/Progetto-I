3c4023f8b4ffcc8e08ec2e36aa910d727e5d51b5
Fabio <Fabio@Fabio-PC>
2016-12-30 15:22:32 +0100

    Rimossa possibilità aggiungere nuovo utente dal login

--- Progetto-I/src/UIlogin/ButtonLayout.java
+++ Progetto-I/src/UIlogin/ButtonLayout.java
@@ -8,7 +8,7 @@ import javax.swing.*;
 import utils.LoginManager;
 
 /**
- * questa classe crea i due pulsanti Login e Nuovo, con relativo listener
+ * Questa classe crea il pulsante Login, con relativo listener
  * @author Luca
  */
 class ButtonLayout extends JPanel{
@@ -39,36 +39,8 @@ class ButtonLayout extends JPanel{
                 }
             }
         });
-        
-        JButton b2 = new JButton("Nuovo");
-        b2.setSize(150, 100);
-        
-        b2.addActionListener(new ActionListener(){
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int var = mgr.getSelectedKey(refRadioPanel.getSelectedButton());
-                boolean flag = false;
                 
-                try {
-                    flag = mgr.checkCredentials(refFormPanel.getUsernameInserted(), refFormPanel.getPasswordInserted(), refRadioPanel.getSelectedButton());
-                } catch (SQLException ex) {
-                    JOptionPane.showMessageDialog(new JFrame(),"Username, password o ruolo errato.","Errore",JOptionPane.ERROR_MESSAGE);
-                }
-                
-                if(var==4){
-                    if(flag){
-                        NewUserWindow win = NewUserWindow.getInstance();    //credenziali confermate quindi lancia utility aggiunta user al database
-                    }
-                }else{
-                    JOptionPane.showMessageDialog(new JFrame(),"Questa funzione è disponibile solo al principale","Info",JOptionPane.INFORMATION_MESSAGE);
-                    refFormPanel.wipePassword();
-                }
-            }
-        });
-        
         this.add(b1);
-        this.add(b2);
     }
     
     /**
